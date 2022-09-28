fd2622cb6d9f30b7a47f05a58e9104bc708d051b
nurseit010101 <nurseit.zhumanazar.uulu@example.com>
2022-09-28 17:08:53 +0600

    second init

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,16 @@
+from multiprocessing import context
 from django.shortcuts import render
+from mainapp.models import *
 
 # Create your views here.
 def main(request):
     return render(request, 'base.html')
 def second(request):
-    return render(request, 'index.html')
+    meals = Meals.objects.all()
+    category = Category.objects.all()
+    context1 = {'meals': meals, 'category': category}
+    return render(request, 'index.html',
+    context=context1)
+    
+    
+
